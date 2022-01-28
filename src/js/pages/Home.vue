3b9cbc7ac5a4fa2f8219429e3438594e7698d165
Navi <mailvoornavi@gmail.com>
2022-01-28 04:54:05 +0100

    added basic styling concept

--- src/js/pages/Home.vue
+++ src/js/pages/Home.vue
@@ -1,22 +1,36 @@
 <template>
-	<p>Count: {{ count }}</p>
-	<p>True Count: {{ trueCount }}</p>
+	<div class="d-flex flex-column align-items-center">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card-content">
+					Count: {{ count }}
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="card-content">
+					True Count: {{ trueCount }}
+				</div>
+			</div>
+		</div>
 
-	<div>
-		<h2>Cards</h2>
-		<button @click="increaseCount">2 - 6</button>
-		<button>7 - 9</button>
-		<button @click="decreaseCount">10 - A</button>
-	</div>
+		<div class="py-3">
+			<h2>Cards</h2>
+			<button class="btn btn-rounded btn-green mx-2" @click="increaseCount">2 - 6</button>
+			<button class="btn btn-rounded btn-green mx-2">7 - 9</button>
+			<button class="btn btn-rounded btn-green mx-2" @click="decreaseCount">10 - A</button>
+		</div>
 
-	<div>
-		<h2>Decks</h2>
-		<button @click="decreaseDeck">-</button>
-		{{ decks }}
-		<button @click="increaseDeck">+</button>
+		<div class="py-3">
+			<h2>Decks</h2>
+			<button class="btn btn-transparent" @click="decreaseDeck">-</button>
+			{{ decks }}
+			<button class="btn btn-transparent" @click="increaseDeck">+</button>
+		</div>
+
+		<button @click="resetCount">Reset</button>
+		
+		<p v-if="trueCount >= 3">You should take insurance.</p>
 	</div>
-	
-	<p v-if="trueCount >= 3">You should take insurance.</p>
 </template>
 
 <script>
@@ -40,6 +54,14 @@
 				this.count += 1
 				this.updateTrueCount()
 			},
+			resetCount() {
+				let text = "Are you sure you want to reset the count?"
+
+				if (confirm(text) == true) {
+					this.count = 0
+					this.updateTrueCount()
+				}
+			},
 			decreaseDeck() {
 				this.decks -= 1
 				this.updateTrueCount()
@@ -50,7 +72,15 @@
 			},
 			updateTrueCount() {
 				this.trueCount = Math.round(this.count / this.decks)
-			}
+			},
 		},
 	}
 </script>
+
+<style scoped>
+.btn-rounded {
+	width: 70px;
+	height: 70px;
+	text-align: center;
+}
+</style>
