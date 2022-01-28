706ab3fe2d9f62cb2d25be9ea0bafb260e68c874
Navi <mailvoornavi@gmail.com>
2022-01-28 15:51:55 +0100

    updated design :)

--- src/js/components/VCounter.vue
+++ src/js/components/VCounter.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="wrapcontent d-flex flex-column align-items-center py-5">
-		<div class="flex-box">
+		<div class="flex-box py-2">
 			<h2>
 				<span class="mr-2"><i class="far fa-dice"></i></span>
 				Blackjack Counter
@@ -8,11 +8,33 @@
 		</div>
 
 		<div class="flex-box py-3">
-			<p>Count: {{ count }}</p>
-			<p>True Count: {{ trueCount }}</p>
+			<div class="card card-soft">
+				<div class="row">
+					<div class="col-md-6">
+						<div class="card-content">
+							<p>
+								True Count
+
+								<span class="float-right tooltip-bottom tooltip-medium" aria-label="Lala">
+									<i class="fal fa-info-circle"></i>
+								</span>
+							</p>
+							<h1>{{ trueCount }}</h1>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="card-content">
+							<p>Odds</p>
+							<h1 v-if="trueCount >= 3">High</h1>
+							<h1 v-if="trueCount <= 2 && trueCount >= 0">Normal</h1>
+							<h1 v-if="trueCount < 0">Low</h1>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
-		<div class="flex-grow-1 py-5">
+		<div class="flex-grow-1 py-3">
 			<div> 
 				<button class="btn btn-rounded btn-green mx-2" @click="increaseCount">2 - 6</button>
 				<button class="btn btn-rounded btn-green mx-2">7 - 9</button>
@@ -21,7 +43,7 @@
 			<p class="hint text-center py-3">Dealt Card</p>
 		</div>
 
-		<div class="flex-grow-1 text-center py-5">
+		<div class="flex-grow-1 text-center py-3">
 			<div>
 				<button class="btn btn-transparent" @click="decreaseDeck">
 					<i class="far fa-minus"></i>
@@ -34,7 +56,7 @@
 			<p class="hint text-center">Total Decks In Game</p>
 		</div>
 
-		<button class="btn btn-block" @click="resetCount">Reset</button>
+		<button class="btn btn-block" @click="attemptResetCount">Reset</button>
 	</div>
 </template>
 
@@ -50,6 +72,10 @@
 			}
 		},
 
+		mounted() {
+			this.resetCount()
+		},
+
 		methods: {
 			decreaseCount() {
 				this.count -= 1
@@ -59,16 +85,20 @@
 				this.count += 1
 				this.updateTrueCount()
 			},
-			resetCount() {
+			attemptResetCount() {
 				let text = "Are you sure you want to reset the count?"
 
 				if (confirm(text) == true) {
-					this.count = 0
-					this.updateTrueCount()
+					this.resetCount()
 				}
 			},
+			resetCount() {
+				this.count = 0
+				this.updateTrueCount()
+			},
 			decreaseDeck() {
-				this.decks -= 1
+				if (this.decks > 1)
+					this.decks -= 1
 				this.updateTrueCount()
 			},
 			increaseDeck() {
@@ -76,7 +106,7 @@
 				this.updateTrueCount()
 			},
 			updateTrueCount() {
-				this.trueCount = Math.round(this.count / this.decks)
+				this.trueCount = (this.count / this.decks).toFixed(2)
 			},
 		},
 	}
@@ -86,5 +116,11 @@
 .flex-box {
 	position: relative;
 	width: 100%;
+}
+
+.card-soft {
+	--card-background-color: 0, 0, 0;
+	--card-foreground-color: 255, 255, 255;
+	border: none;
 }
 </style>
